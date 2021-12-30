ad5a43afab42b6b189286816c41f5332e8841034
Talib <khantalib707@gmail.com>
2021-12-30 09:07:37 +0530

    Second Commit in Student.java

--- src/main/java/com/springcore/Student.java
+++ src/main/java/com/springcore/Student.java
@@ -1,1 +1,51 @@
+package com.springcore;
 
+public class Student {
+	
+	private int stuId;
+	private String stuName;
+	private String stuAddres;
+	
+	public Student() {
+		super();
+		
+	}
+
+	public Student(int stuId, String stuName, String stuAddres) {
+		super();
+		this.stuId = stuId;
+		this.stuName = stuName;
+		this.stuAddres = stuAddres;
+	}
+
+	public int getStuId() {
+		return stuId;
+	}
+
+	public void setStuId(int stuId) {
+		this.stuId = stuId;
+	}
+
+	public String getStuName() {
+		return stuName;
+	}
+
+	public void setStuName(String stuName) {
+		this.stuName = stuName;
+	}
+
+	public String getStuAddres() {
+		return stuAddres;
+	}
+
+	public void setStuAddres(String stuAddres) {
+		this.stuAddres = stuAddres;
+	}
+
+	@Override
+	public String toString() {
+		return " [stuId=" + stuId + ", stuName=" + stuName + ", stuAddres=" + stuAddres + "]";
+	}
+	
+
+}
